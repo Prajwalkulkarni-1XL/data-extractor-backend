1bdee5dc63c0c09708f0ed7d046f3851b7c7ec2f
Prajwal <prajwal.kulkarni@1xl.com>
2025-08-28 16:07:02 +0530

    created https certificate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import https from "https";
+import fs from "fs";
 dotenv.config({
   path: "./.env",
 });
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+
+    // Self-signed SSL certificate for local testing
+    const sslOptions = {
+      key: fs.readFileSync("./certs/private.key"),
+      cert: fs.readFileSync("./certs/certificate.crt"),
+    };
+
+    https.createServer(sslOptions, app).listen(process.env.PORT, () => {
+      console.log(`🔒 HTTPS server running at https://localhost:${process.env.PORT}`);
     });
+
+    // app.listen(process.env.PORT || 8000, () => {
+    //   console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    // });
   })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
