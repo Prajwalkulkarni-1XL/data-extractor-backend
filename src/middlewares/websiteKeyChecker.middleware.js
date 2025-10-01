ff2f1682bedf3c6d37ce9bde3856665515821bfe
Prajwal <prajwal.kulkarni@1xl.com>
2025-10-01 11:18:24 +0530

    fixed website key middleware issue

--- src/middlewares/websiteKeyChecker.middleware.js
+++ src/middlewares/websiteKeyChecker.middleware.js
@@ -5,7 +5,7 @@ export const websiteKeyChecker = asyncHandler((req, res, next) => {
 
   console.log(websiteKey)
 
-  if (!websiteKey || websiteKey !== null || websiteKey !== undefined || websiteKey === ":websiteKey") {
+  if (!websiteKey || websiteKey === null || websiteKey === "null" || websiteKey === undefined || websiteKey === "undefined" || websiteKey === ":websiteKey") {
     return res.status(400).json({ success: false, message: "Website key cannot be null or undefined or :websiteKey" });
   }
 
